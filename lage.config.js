a1977ef2310d72d2dfd4a19701cdefd459376249
Ken Chau <kchau@microsoft.com>
2024-02-16 15:52:17 -0800

    noop

--- lage.config.js
+++ lage.config.js
@@ -5,19 +5,34 @@ module.exports = {
       type: "noop",
       dependsOn: ["tsc", "webpack"],
       inputs: [],
+      outputs: [],
+      cache: false,
     },
     webpack: {
+      type: "noop",
+      dependsOn: ["webpack:esm", "webpack:cjs"],
+      inputs: [],
+      outputs: [],
+      cache: false,
+    },
+    "webpack:esm": {
       type: "npmScript",
       dependsOn: [],
-      inputs: ["src/**/*"]
+      inputs: ["src/**/*"],
+    },
+    "webpack:cjs": {
+      type: "npmScript",
+      dependsOn: [],
+      inputs: ["src/**/*"],
     },
     tsc: {
       type: "npmScript",
       dependsOn: [],
-      inputs: ["src/**/*"]
+      inputs: ["src/**/*"],
     },
   },
   cacheOptions: {
-
+    environmentGlob: ["*.js", "*.json"],
+    outputGlob: ["dist/**", "lib/**"],
   },
 };
